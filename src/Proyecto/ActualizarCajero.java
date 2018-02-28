ae15e234e391b327328972c476dab88c8889a75d
Robert Gonzalez <robert_gon97@hotmail.com>
2018-02-28 19:49:58 -0400

    Refactorizando Codigo

--- src/Proyecto/ActualizarCajero.java
+++ src/Proyecto/ActualizarCajero.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Proyecto;
 
 import com.mysql.jdbc.Connection;
@@ -14,290 +9,139 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * 
- */
 public class ActualizarCajero {
-    
-    
     String conString ="jdbc:mysql://localhost:3306/sistemainventario";
     String username ="root";
     String passward ="";
     
-    /*public boolean searchemp(){
-        
-        String sql = "SELECT * FROM  `cajero`  WHERE  `reason` ='employee payment'"; 
-        
-        try{
-            
-            Connection con= (Connection) DriverManager.getConnection(conString, username, passward);
-            
-            PreparedStatement s =(PreparedStatement) con.prepareStatement(sql);
-            
-            ResultSet rs =s.executeQuery(sql);
-            
-            if(rs.next()){
-                return true;
-            }
-            
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    
-        return false;
-    }*/
-    
-    
     public boolean search(String a){
-        
         String sql = "SELECT * FROM  `cajero`  WHERE  `motivo` ='"+a+"'"; 
-        
         try{
-            
             Connection con= (Connection) DriverManager.getConnection(conString, username, passward);
-            
             PreparedStatement s =(PreparedStatement) con.prepareStatement(sql);
-            
             ResultSet rs =s.executeQuery(sql);
-            
             if(rs.next()){
                 return true;
             }
-            
         }catch(Exception e){
             e.printStackTrace();
         }
-    
         return false;
     }
-    
     
     public float employeemoney(){
-        
         float mon = 0;
-        
         String sql = "SELECT * FROM  `pagoempleados`";
-        
         try{
-            
             Connection con= (Connection) DriverManager.getConnection(conString, username, passward);
-            
             PreparedStatement s =(PreparedStatement) con.prepareStatement(sql);
-            
             ResultSet rs =s.executeQuery(sql);
-            
-            
-            
             while(rs.next()){
-                
                 String money = rs.getString(3);
-                
                 mon = mon + Float.parseFloat(money);
-                
-                
             }
-            
-            
-            
             return mon;
-            
-            
         }catch(Exception e){
             e.printStackTrace();
         }
-        
         return mon;
-        
     }
     
-    
     public float slodmoney(){
-        
         float mon = 0;
-        
         String sql = "SELECT * FROM  `ventas`";
-        
         try{
-            
             Connection con= (Connection) DriverManager.getConnection(conString, username, passward);
-            
             PreparedStatement s =(PreparedStatement) con.prepareStatement(sql);
-            
             ResultSet rs =s.executeQuery(sql);
-            
-            
-            
             while(rs.next()){
-                
                 String qua = rs.getString(3);
-                
                 String money = rs.getString(4);
-                
                 mon = mon + Float.parseFloat(money)*Integer.parseInt(qua);
-                
-                
             }
-            
-            
-            //System.out.println(mon);
             return mon;
-            
-            
         }catch(Exception e){
             e.printStackTrace();
         }
-        
         return mon;
-        
     }
     
     public float buymoney(){
-        
         float mon = 0;
-        
         String sql = "SELECT * FROM  `ventas`";
-        
-        try{
-            
+        try {
             Connection con= (Connection) DriverManager.getConnection(conString, username, passward);
-            
             PreparedStatement s =(PreparedStatement) con.prepareStatement(sql);
-            
             ResultSet rs =s.executeQuery(sql);
-            
-            
-            
             while(rs.next()){
-                
                 String qua = rs.getString(3);
-                
                 String money = rs.getString(7);
-                
                 mon = mon + Float.parseFloat(money)*Integer.parseInt(qua);
-                
-                
             }
-            
-            
-            //System.out.println(mon);
             return mon;
-            
-            
         }catch(Exception e){
             e.printStackTrace();
         }
-        
         return mon;
-        
     }
-    
-    
-    
     
     public void update(String reason,float money){
-        
-        
         Date dNow = new Date( );
-                    
         SimpleDateFormat ft = new SimpleDateFormat ("yyyy.MM.dd");
         String date = ""+ft.format(dNow);
-        
         String sql = "UPDATE `cajero` SET `dinero`='"+money+"',`fecha`='"+date+"' WHERE `motivo` ='"+reason+"'"; 
-        
         try{
-            
             Connection con= (Connection) DriverManager.getConnection(conString, username, passward);
-            
             PreparedStatement s =(PreparedStatement) con.prepareStatement(sql);
-            
             s.execute(sql);
-            
-            
-            
         }catch(Exception e){
             e.printStackTrace();
         }
-        
     }
-    
     
     public void add(String cinout,String reason,float money){
-        
         Date dNow = new Date( );
-                    
         SimpleDateFormat ft = new SimpleDateFormat ("yyyy.MM.dd");
         String date = ""+ft.format(dNow);
-        
         String sql = "INSERT INTO `cajero`(`id`, `tipo`, `motivo`, `dinero`, `fecha`) VALUES (NULL,'"+cinout+"','"+reason+"','"+money+"','"+date+"')"; 
-        
         try{
-            
             Connection con= (Connection) DriverManager.getConnection(conString, username, passward);
-            
             PreparedStatement s =(PreparedStatement) con.prepareStatement(sql);
-            
             s.execute(sql);
-            
-            
-            
         }catch(Exception e){
             e.printStackTrace();
         }
- 
-        
     }
     
-    
     public DefaultTableModel getData(){
-        
         DefaultTableModel dm = new DefaultTableModel();
-        
         dm.addColumn("Fecha");
         dm.addColumn("Tipo Entrada/Salida");
         dm.addColumn("Motivo");
         dm.addColumn("Dinero");
-        
-        
         String sql = "SELECT * FROM `cajero`";
-        
         try{
             Connection con= (Connection) DriverManager.getConnection(conString, username, passward);
-            
             Statement s =(Statement) con.prepareStatement(sql);
-            
             ResultSet rs =s.executeQuery(sql);
-            
             while(rs.next()){
-               
                 String d = rs.getString(5);
                 String c = rs.getString(2);
                 String r = rs.getString(3);
                 String m = rs.getString(4);
-                
                 dm.addRow(new String[]{d,c,r,m});
-                
             }
-            
             return dm;
-            
         }catch(Exception e){
             e.printStackTrace();
         }
-        
         return null;
     }
     
-    
-    
     public boolean drop(){
-        
         Date dNow = new Date( );
-                    
         SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd");
-    
         String date= ""+ft.format(dNow);
-        
         String sql1 = "DELETE FROM pagoempleados"; 
         String sql2 = "DELETE FROM ventas";
         String sql3 = "DELETE FROM fechaventa";
@@ -310,11 +154,8 @@ public class ActualizarCajero {
         String sql10 = "ALTER TABLE cajero AUTO_INCREMENT = 1";
         String sql11 = "ALTER TABLE ventash AUTO_INCREMENT = 1";
         String adddate = "INSERT INTO `ventash`(`numero`, `producto`, `cantidad`, `precio`, `precio_compra`, `tipo`, `fecha`) VALUES (NULL,'a','0','0','0','NULL','"+date+"')";
-        
         try{
-            
             Connection con= (Connection) DriverManager.getConnection(conString, username, passward);
-            
             PreparedStatement s1 =(PreparedStatement) con.prepareStatement(sql1);
             PreparedStatement s2 =(PreparedStatement) con.prepareStatement(sql2);
             PreparedStatement s3 =(PreparedStatement) con.prepareStatement(sql3);
@@ -327,9 +168,6 @@ public class ActualizarCajero {
             PreparedStatement s10 =(PreparedStatement) con.prepareStatement(sql10);
             PreparedStatement s11 =(PreparedStatement) con.prepareStatement(sql11);
             PreparedStatement s =(PreparedStatement) con.prepareStatement(adddate);
-            
-            
-            
             s1.execute(sql1);
             s2.execute(sql2);
             s3.execute(sql3);
@@ -342,46 +180,29 @@ public class ActualizarCajero {
             s10.execute(sql10);
             s11.execute(sql11);
             s.execute(adddate);
-            
             return true;
-            
-            
         }catch(Exception e){
             e.printStackTrace();
         }
-    
         return false;
     }
     
-    
     public String getStartDate(){
-        
         String sql = "SELECT * FROM `fechaventa`";
         String d="";
         try{
             Connection con= (Connection) DriverManager.getConnection(conString, username, passward);
-            
             Statement s =(Statement) con.prepareStatement(sql);
-            
             ResultSet rs =s.executeQuery(sql);
-            
             if(rs.next()){
-               
                 d = rs.getString(6);
                 //d.substring(11, d.length());
                 d=d.substring(0, 10);
-                
-                
             }
-            
             return d;
-            
         }catch(Exception e){
             e.printStackTrace();
         }
-        
         return d;
     }
-    
-    
 }
