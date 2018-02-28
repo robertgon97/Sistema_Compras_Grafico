63d356abe3e3e8fb92d5c56217a33b67c9757c11
Robert Elias Gonzalez Oropeza <robert_gon97@hotmail.com>
2018-02-28 14:10:16 -0400

    Refactorizando codigo

--- src/Proyecto/ProjectFinal.java
+++ src/Proyecto/ProjectFinal.java
@@ -1,44 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Proyecto;
 
 import javax.swing.UIManager;
-
-/**
- *
- * 
- */
 public class ProjectFinal {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-    
-    new Thread(){
-       public void run() {
-                
+        new Thread(){
+            public void run() {
                 try {
                     Thread.sleep(3500);
-                    System.out.println("dd");
+                    System.out.println("Inicio de la App");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                
                 new LogIn().setVisible(true);
             }
-       
-    }.start();
+        }.start();
         try {
-            
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            
         } catch (Exception ex) {
-            
+            System.out.println("Error" + ex.toString());
         }
     }
-    
 }
